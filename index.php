f5d55961d565e422f08b3a5d0359a55ec8621c85
Leandr0Arantes <leandro.arantes@edu.uniube.br>
2025-02-26 13:10:38 -0300

    Atualização index

--- index.php
+++ index.php
@@ -3,15 +3,36 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/Assets/css/index.css">
+    <link rel="stylesheet" href="assets/css/index.css">
     <title>TaskMaker</title>
 </head>
 <body>
-    <div class="container">
-        <h1>TaskMaker</h1>
-    <a href="pages/login.php">login</a>
-    <a href="pages/cadastro.php">cadastro de usuário</a>
-    </div>
+    <header>
+        <div class="logo-taskmaker">
+            <a href="index.php">Taskmaker</a>
+        </div>
+        <nav>
+            <ul>
+                <li><a href="#">Welcome</a></li>
+                <li><a href="#">Teste</a></li>
+                <li><a href="#">Teste</a></li>
+                <li><a href="#">Contato</a></li>
+            </ul>
+        </nav>
+        <div class="btn-header">
+            <a href="pages/login.php">Log in</a>
+            <a class="sign-in" href="pages/cadastro.php">Sign in</a>
+        </div>
+    </header>
+    <section class="welcome">
+        <form action="" method="post">
+            <input type="text">
+        </form>
+    </section>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga laborum nesciunt dolore beatae voluptatum minus tenetur nemo, sequi, culpa id natus ad iure officia illo? Iste sed quam mollitia dicta!</p>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga laborum nesciunt dolore beatae voluptatum minus tenetur nemo, sequi, culpa id natus ad iure officia illo? Iste sed quam mollitia dicta!</p>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga laborum nesciunt dolore beatae voluptatum minus tenetur nemo, sequi, culpa id natus ad iure officia illo? Iste sed quam mollitia dicta!</p>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga laborum nesciunt dolore beatae voluptatum minus tenetur nemo, sequi, culpa id natus ad iure officia illo? Iste sed quam mollitia dicta!</p>
 </body>
 
 <?php
